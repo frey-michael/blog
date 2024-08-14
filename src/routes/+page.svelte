8cd3e3264b9b7f93e892d3eafc1b22b0d6825a70
Kesseline <kessel.13@outlook.com>
2024-08-14 21:03:35 +0200

    update styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,26 +11,59 @@
 
 <label for="searchInput">Search</label>
 <input bind:value={val} type="text" id="searchInput" />
+<a href="https://www.github.com/Kesseline" 
+	><img src="src/assets/github_logo.png" alt="Github Logo" class="links"/></a
+>
 
 <div class="tile-grid">
 	{#each $filtered as item}
-		<div class="tile">{item}</div>
+		<div class="tile">
+			{item}
+			<div class="summary">
+				Curabitur velit leo, facilisis sit amet venenatis ut, convallis a nisl. Pellentesque
+				habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque
+				fringilla vel arcu vitae faucibus. Maecenas ut fermentum dui.
+			</div>
+		</div>
 	{/each}
 </div>
 
-<a href="https://www.github.com/Kesseline"
-	><img src="src/assets/github_logo.png" alt="Github Logo" /></a
->
 
 <style>
+	:global(body) {
+		background-color: #b39546;
+		font-family: 'Lucida Console', 'Courier New', monospace;
+	}
+
 	.tile-grid {
 		display: grid;
-		grid-gap: 15px;
+		grid-gap: 25px;
 		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	}
 
 	.tile {
-		border: 1px solid black;
-		min-height: 50px;
+		color: black;
+		font-size: larger;
+		border-radius: 25px;
+		text-align: center;
+		padding-top: 20px;
+		margin-top: 50px;
+		background-color: #8a5829;
+		border: 2px solid black;
+		min-height: 200px;
 	}
+
+	.summary {
+		margin-top: 50px;
+		margin-bottom: 15px;
+		font-size: medium;
+	}
+
+  .links {
+    max-height: 50px;
+    position: fixed;
+    right: 100px;
+    top:20px;
+    
+  }
 </style>
