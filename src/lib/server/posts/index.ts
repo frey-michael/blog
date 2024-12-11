e0093fb8352e08d25318e0db96c9e77ad8be8365
Kesseline <kessel.13@outlook.com>
2024-12-11 17:21:29 +0100

    Add corrections

--- src/lib/server/posts/index.ts
+++ src/lib/server/posts/index.ts
@@ -1,5 +1,3 @@
-import {compile} from "mdsvex"
-
 export const fetchMarkdownPosts = async () => {
   const allPostFiles = import.meta.glob('/src/lib/server/posts/*.md');
   const iterablePostFiles = Object.entries(allPostFiles);
@@ -9,23 +7,12 @@ export const fetchMarkdownPosts = async () => {
       const {metadata} = await resolver();
       const postPath = path.slice(11, -3);
       const post = await resolver()
-      const text = `
-        # Test Markdown
-
-        blub blyub
-
-        asdasdjk,
-        `
-      const content = await compile(text)
-      const content2 = post.default.render()
-
-      console.log(post)
-      console.log(metadata)
+      const content = post.default.render()
 
       return {
         meta: metadata,
         path: postPath,
-        text: content2,
+        text: content,
       };
     })
   );
