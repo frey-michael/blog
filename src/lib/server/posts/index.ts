1917572458580c886240219d96e81f6009bd069c
Michael Frey <kessel.13@outlook.com>
2026-04-26 17:29:18 +0200

    add post

--- src/lib/server/posts/index.ts
+++ src/lib/server/posts/index.ts
@@ -4,8 +4,9 @@ import * as post1 from './post1.md';
 import * as post2 from './post2.md';
 import * as post3 from './post3.md';
 import * as post4 from './post4.md';
+import * as post5 from './post5.md';
 
-const allPostFiles = [post1, post2, post3, post4]
+const allPostFiles = [post1, post2, post3, post4, post5]
 
 const allPosts =
   allPostFiles.map(async post => {
